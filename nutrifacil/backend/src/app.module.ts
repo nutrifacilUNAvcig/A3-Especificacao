f52ce3c72973beddd79491d8e1a6edfd04dfe696
Isaque02 <isaquezulato@gmail.com>
2025-06-11 22:22:09 -0300

    Resolvendo conflitos da dependencias do mongoose

--- nutrifacil/backend/src/app.module.ts
+++ nutrifacil/backend/src/app.module.ts
@@ -1,17 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { DietasModule } from './dietas/dietas.module';
 import { CalculosModule } from './calculos/calculos.module';
-import { UsuariosController } from './usuarios/usuarios.controller';
-import { DietasController } from './dietas/dietas.controller';
-import { CalculosController } from './calculos/calculos.controller';
-import { UsuariosService } from './usuarios/usuarios.service';
-import { DietasService } from './dietas/dietas.service';
-import { CalculosService } from './calculos/calculos.service';
 
 @Module({
-  imports: [UsuariosModule, DietasModule, CalculosModule],
-  controllers: [UsuariosController, DietasController, CalculosController],
-  providers: [UsuariosService, DietasService, CalculosService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nutrifacil'),
+  UsuariosModule, 
+  DietasModule, 
+  CalculosModule,
+  ],
 })
 export class AppModule {}
