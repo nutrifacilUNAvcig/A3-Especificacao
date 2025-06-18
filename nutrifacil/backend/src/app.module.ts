4118c7c3242475b3e7c30151ccac117c73ddd0b7
Vitor Jau <vitor.jau@dtidigital.com.br>
2025-06-18 17:40:50 -0300

    arrumando e corrindo erro integracao front e back

--- nutrifacil/backend/src/app.module.ts
+++ nutrifacil/backend/src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 //import { MongooseModule } from '@nestjs/mongoose';
-import { UsuariosModule } from './usuarios/usuarios.module';
+//import { UsuariosModule } from './usuarios/usuarios.module';
 import { DietasModule } from './dietas/dietas.module';
 import { CalculosModule } from './calculos/calculos.module';
 
 @Module({
   imports: [
    //MongooseModule.forRoot('mongodb://localhost:27017/nutrifacil'),
-  UsuariosModule, 
+  //UsuariosModule, 
   DietasModule, 
   CalculosModule,
   ],
